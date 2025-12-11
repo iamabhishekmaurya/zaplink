21d6840e23b7000de7fdbbb65152d7370aae561b
iamabhishekmaurya <iamabhishekmaurya@gmail.com>
2025-12-11 21:44:11 +0530

    update postgress config and add UI code

--- services/zaplink-notification-service/src/main/java/io/zaplink/notification/config/KafkaConfig.java
+++ services/zaplink-notification-service/src/main/java/io/zaplink/notification/config/KafkaConfig.java
@@ -57,19 +57,19 @@ public class KafkaConfig
         return factory;
     }
 
-    @Bean
-    public ProducerFactory<String, Object> producerFactory()
-    {
-        Map<String, Object> configProps = new HashMap<>();
-        configProps.put( ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers );
-        configProps.put( ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class );
-        configProps.put( ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JacksonJsonDeserializer.class );
-        return new DefaultKafkaProducerFactory<>( configProps );
-    }
+    // @Bean
+    // public ProducerFactory<String, Object> producerFactory()
+    // {
+    //     Map<String, Object> configProps = new HashMap<>();
+    //     configProps.put( ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers );
+    //     configProps.put( ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class );
+    //     configProps.put( ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JacksonJsonDeserializer.class );
+    //     return new DefaultKafkaProducerFactory<>( configProps );
+    // }
 
-    @Bean
-    public KafkaTemplate<String, Object> kafkaTemplate()
-    {
-        return new KafkaTemplate<>( producerFactory() );
-    }
+    // @Bean
+    // public KafkaTemplate<String, Object> kafkaTemplate()
+    // {
+    //     return new KafkaTemplate<>( producerFactory() );
+    // }
 }
