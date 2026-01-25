464466c69816427af52f152526247dd66e0b6cd7
iamabhishekmaurya <iamabhishekmaurya@gmail.com>
2026-01-25 21:55:03 +0530

    initial setup of redirect service

---
+++ services/zaplink-redirect-service/src/main/java/io/zaplink/redirect/ZaplinkRedirectServiceApplication.java
@@ -0,0 +1,13 @@
+package io.zaplink.redirect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaplinkRedirectServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZaplinkRedirectServiceApplication.class, args);
+	}
+
+}
---
+++ services/zaplink-redirect-service/src/test/java/io/zaplink/redirect/ZaplinkRedirectServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.zaplink.redirect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZaplinkRedirectServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
