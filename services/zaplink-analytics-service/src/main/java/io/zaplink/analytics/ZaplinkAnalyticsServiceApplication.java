1a6fe1dd8841110df62e1f743c10c6c067918131
iamabhishekmaurya <iamabhishekmaurya@gmail.com>
2026-01-30 17:57:03 +0530

    analytics service initial setup

---
+++ services/zaplink-analytics-service/src/main/java/io/zaplink/analytics/ZaplinkAnalyticsServiceApplication.java
@@ -0,0 +1,13 @@
+package io.zaplink.analytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaplinkAnalyticsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZaplinkAnalyticsServiceApplication.class, args);
+	}
+
+}
---
+++ services/zaplink-analytics-service/src/test/java/io/zaplink/analytics/ZaplinkAnalyticsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.zaplink.analytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZaplinkAnalyticsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
